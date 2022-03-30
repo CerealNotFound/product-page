0c52b83ed9bb3890f174a01b041c269b3d82d97b
Adarsh <hydrogenproton1@gmail.com>
2022-03-31 00:16:34 +0530

    Fixed bug

--- src/components/Modals/CartModal/CartModal.js
+++ src/components/Modals/CartModal/CartModal.js
@@ -5,8 +5,8 @@ import "../../styles/css/CartModal.css";
 
 const CartModal = () => {
 
-    const [forceUpdate] = useReducer(x => x + 1, 0);
-        
+    const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
+
     return ReactDOM.createPortal(
                 <div id="cart-wrapper-modal">
                     <h4 id="cart-heading">Cart</h4>
